ef2f2c29f770ec970cfc63e1fee4fe2ee6a960c7
rythomas-usaid <143015628+rythomas-usaid@users.noreply.github.com>
2024-03-06 09:01:56 -0500

    move read_... functions to read_indicator

--- R/rbind_dis_to_ftfms.R
+++ R/rbind_dis_to_ftfms.R
@@ -1,127 +1,20 @@
 #' parse DIS extracts into usable data
 #'
 #' create data set from DIS exports and FTFMS data
-#' @param input_dir a directory with raw ftfms and dis (extract) data
-#' @param output_dir a directory to store outputs
+#' @param ftfms_input_dir character. Directory path to ftfms Excel files
+#' @param dis_input character. File path of DIS extract CSV.
+#' @param output_dir character. Directory path to store outputs combined_extracts.csv and combined_extracts.Rdata
 #' @return data.frame with combined ftfms and DIS data
 #' @examples
 #' #' combine_extracts()
 #' @import tidyverse
 #'
 #' @export rbind_dis_to_ftfms
+rbind_dis_to_ftfms <- function(dis_input, ftfms_input_dir = "../../indicators/extract/", output_dir = "../data/") {
+  dis <- read_dis(input_dir = dis_input)
+  ms <- read_ftfms(input_dir = ftfms_input_dir)
 
-rbind_dis_to_ftfms <- function(input_dir = "../../indicators/extract/"
-                               , output_dir = "../data/") {
-
-  # gs4_auth()
-  # load("../database/extract/indicators_db.rdata")
-  print("Reading DIS extract ... ")
-  dat <- data.table::fread( # data.table is faster at reading large files
-    paste0(input_dir
-           , "DIS ENT - OU Activity Indicator Results (All Data)_20230622 Extract_Extract.csv")
-    , colClasses=c("Activity.Code"="character")
-    , na.strings = "") %>% as.data.frame() %>%
-    rename_extract_columns()
-
-  # print("Changing all characters to lower case ...")
-  # make sure all the values are lowercase
-  # dat[, sapply(dat, is.character)] <- lapply(dat[, sapply(dat, is.character)], tolower)
-
-  dat$system <- "dis"
-
-
-  # make_uic <- function(ic) {
-  #   ifelse(ic %in% c("eg.3.2-24", "eg.3.2-x17"), "eg.3.2-24/eg.3.2-x17"
-  #   , ifelse(ic %in% c("eg.3.2-25","eg.3.2-x18"), "eg.3.2-25/eg.3.2-x18"
-  #   , ifelse(ic %in% c("eg.3.2-26", "eg.3.2-x19"), "eg.3.2-26/eg.3.2-x19"
-  #   , ifelse(ic %in% c("eg.3.2-27","eg.3.2-x6"), "eg.3.2-27/eg.3.2-x6"
-  #   , ifelse(ic %in% c("eg.3.1-14/-15","eg.3.2-x22"),"eg.3.1-14/-15/eg.3.2-x22"
-  #   , ic)))))
-  # }
-
-  # dat$uic <- make_uic(dat$ic)
-  dat$actual <- as.numeric(dat$actual)
-  dat$target <- as.numeric(dat$target)
-  dat$uudn <- paste(dat$ic, dat$udn, sep = "_")
-  dat$commodity <- trimws(dat$commodity)
-
-  # dcw <- make_dis_crosswalk() #%>%
-  #   dplyr::mutate(ic = stringr::str_to_lower(ic))
-
-  # commodity_udns <- dcw %>%
-  #   dplyr::filter(if_any(ends_with("order"), ~ stringr::str_detect(., "\\["))) %>%
-  #   dplyr::distinct(ic, udn) %>%
-  #   dplyr::left_join(
-  #     dplyr::select(
-  #       dplyr::filter(
-  #         dat, !is.na(commodity)), ic, udn, commodity) %>%
-  #       dplyr::distinct())
-
-  dat$id <- NULL
-  # dat$first_order <- NULL
-  # dat$second_order <- NULL
-  # dat$third_order <- NULL
-  # dat$fourth_order <- NULL
-
-  dat <- as_tibble(dat) %>% left_join(dcw, relationship = "many-to-one")
-
-
-  ## Read FTFMS data ####
-  ms1 <- openxlsx::read.xlsx(paste0(input_dir, "ftfms/FTFMS Full Dataset 2011-2019 1.xlsx"))
-  ms1$DisaggregationName5 <- as.character(ms1$DisaggregationName5)
-  ms2 <- openxlsx::read.xlsx(paste0(input_dir, "ftfms/FTFMS Full Dataset 2011-2019 2.xlsx"))
-  ms2$DisaggregationName5 <- as.character(ms2$DisaggregationName5)
-
-  ms <- rbind(ms1, ms2)
-  rm(ms1, ms2)
-
-  ms$system <- "ms"
-
-  names(ms) <- tolower(names(ms))
-
-  split_columns <- strsplit(ms$indicatorname, ": ")
-  result_df <- data.frame(
-    ic = sapply(split_columns, `[`, 1),
-    i_name = sapply(split_columns, `[`, 2)
-  )
-  ms$ic <- result_df$ic
-  ms$i_name <- result_df$i_name
-  ms$indicatorname <- NULL
-  rm(result_df)
-
-  names(ms)[names(ms) == "reportingorganization"] <- "ro"
-  names(ms)[names(ms) == "bureau"] <- "m_office"
-  names(ms)[names(ms) == "operatingunit"] <- "ou"
-  names(ms)[names(ms) == "bureau/region" ] <- "a_code"
-  names(ms)[names(ms) == "pa-id"] <- "program"
-  names(ms)[names(ms) == "country.(usda)" ] <- "country"
-  names(ms)[names(ms) == "disaggregationname1"] <- "ms_d1"
-  names(ms)[names(ms) == "disaggregationname2" ] <- "ms_d2"
-  names(ms)[names(ms) == "disaggregationname3"] <- "ms_d3"
-  names(ms)[names(ms) == "disaggregationname4" ] <- "ms_d4"
-  names(ms)[names(ms) == "disaggregationname5"] <- "ms_d5"
-  names(ms)[names(ms) == "targetvalue"] <- "target"
-  names(ms)[names(ms) == "actualvalue"] <- "actual"
-  names(ms)[names(ms) == "deviationnarrative"] <- "deviation.narrative"
-
-  #as_tibble(ms)
-  ms <- ms %>%
-    dplyr::mutate(dplyr::across(c(year, baselineyear),as.integer)
-           , dplyr::across(c(target, actual), as.numeric))
-
-
-  # make ordered disaggregates
-  ms <- ms %>%
-    dplyr::mutate(d1 = first_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
-                  , d2 = second_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
-                  , d3 = third_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
-                  , d4 = fourth_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
-                  , indicator.collection.frequency = "annual")
-  #distinct(ms, ic, first_order, second_order, third_order, fourth_order)
-
-  #dat[, setdiff(names(ms), names(dat))] <- NA
-
-  df <- dplyr::bind_rows(dat, ms) %>%
+  df <- dplyr::bind_rows(dis, ms) %>%
     dplyr::mutate(uic = make_uic(tolower(ic), tolower(d1), tolower(d2)))
 
   write.csv(df, paste0(output_dir, "combined_extracts.csv"))
@@ -129,3 +22,4 @@ rbind_dis_to_ftfms <- function(input_dir = "../../indicators/extract/"
   # return(df)
 
 }
+
--- R/read_indicator.R
+++ R/read_indicator.R
@@ -21,18 +21,156 @@ read_indicator <- function(x, sheet = "IM Full Disaggs", format) {
       }
   }
 
-# openxlsx::read.xlsx( mapper$indicator_files[1], sheet = mapper$worksheet_names[1], fillMergedCells=T) %>%
-#   tidyr::pivot_longer(tidyselect::starts_with("Actual") | tidyselect::starts_with("Target")) %>%
-#   tidyr::separate(name, into = c("type", "year")) %>%
-#   dplyr::mutate(across(-value, ~ trimws(.)))
 
-#' @export read_export
-# library(tidyverse)
-read_export <- function(x) {
-  openxlsx::read.xlsx(x) %>%
-    mutate(across(c(Target, Actual), ~ parse_double(gsub("[%USD,$ -]", "", .)))) %>%
-    rename_extract_columns()
+# @export read_export
+# This function may not necessary. Test later to confirm.
+# read_export <- function(x) {
+#   openxlsx::read.xlsx(x) %>%
+#     mutate(across(c(Target, Actual), ~ parse_double(gsub("[%USD,$ -]", "", .)))) %>%
+#     rename_extract_columns()
+# }
+
+#' parse DIS extracts into usable data
+#'
+#' create data set from DIS exports and FTFMS data
+#' @param input_dir a directory with raw ftfms and dis (extract) data
+#' @return data.frame with combined ftfms data
+#' @examples
+#' #' ms <- read_ftfms()
+#' @import tidyverse
+#'
+#' @export read_ftfms
+read_ftfms <- function(input_dir = "../../indicators/extract/") {
+
+  ms1 <- openxlsx::read.xlsx(paste0(input_dir, "ftfms/FTFMS Full Dataset 2011-2019 1.xlsx"))
+  ms1$DisaggregationName5 <- as.character(ms1$DisaggregationName5)
+  ms2 <- openxlsx::read.xlsx(paste0(input_dir, "ftfms/FTFMS Full Dataset 2011-2019 2.xlsx"))
+  ms2$DisaggregationName5 <- as.character(ms2$DisaggregationName5)
+
+  ms <- rbind(ms1, ms2)
+  rm(ms1, ms2)
+
+  ms$system <- "ms"
+
+  names(ms) <- tolower(names(ms))
+
+  split_columns <- strsplit(ms$indicatorname, ": ")
+  result_df <- data.frame(
+    ic = sapply(split_columns, `[`, 1),
+    i_name = sapply(split_columns, `[`, 2)
+  )
+  ms$ic <- result_df$ic
+  ms$i_name <- result_df$i_name
+  ms$indicatorname <- NULL
+  rm(result_df)
+
+  names(ms)[names(ms) == "reportingorganization"] <- "ro"
+  names(ms)[names(ms) == "bureau"] <- "m_office"
+  names(ms)[names(ms) == "operatingunit"] <- "ou"
+  names(ms)[names(ms) == "bureau/region" ] <- "a_code"
+  names(ms)[names(ms) == "pa-id"] <- "program"
+  names(ms)[names(ms) == "country.(usda)" ] <- "country"
+  names(ms)[names(ms) == "disaggregationname1"] <- "ms_d1"
+  names(ms)[names(ms) == "disaggregationname2" ] <- "ms_d2"
+  names(ms)[names(ms) == "disaggregationname3"] <- "ms_d3"
+  names(ms)[names(ms) == "disaggregationname4" ] <- "ms_d4"
+  names(ms)[names(ms) == "disaggregationname5"] <- "ms_d5"
+  names(ms)[names(ms) == "targetvalue"] <- "target"
+  names(ms)[names(ms) == "actualvalue"] <- "actual"
+  names(ms)[names(ms) == "deviationnarrative"] <- "deviation.narrative"
+
+  #as_tibble(ms)
+  ms <- ms %>%
+    dplyr::mutate(dplyr::across(c(year, baselineyear),as.integer)
+                  , dplyr::across(c(target, actual), as.numeric))
+
+
+  # make ordered disaggregates
+  ms <- ms %>%
+    dplyr::mutate(d1 = first_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
+                  , d2 = second_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
+                  , d3 = third_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
+                  , d4 = fourth_order(ic, ms_d1, ms_d2, ms_d3, ms_d4)
+                  , indicator.collection.frequency = "annual")
+  #distinct(ms, ic, first_order, second_order, third_order, fourth_order)
+
+  #dat[, setdiff(names(ms), names(dat))] <- NA
+
+  return(ms)
+
+
 }
+
+
+#' parse DIS extracts into usable data
+#'
+#' create data set from DIS exports and FTFMS data
+#' @param input_dir a directory with raw dis (extract CSV) data
+#' @param output_dir a directory to store outputs
+#' @return data.frame with combined DIS data
+#' @examples
+#' #' dis <- read_dis()
+#' @import tidyverse
+#'
+#' @export read_dis
+read_dis <- function(input) {
+
+  disaggregate_crosswalk <- make_disaggregate_crosswalk()
+
+  # load("../database/extract/indicators_db.rdata")
+  print("Reading DIS extract ... ")
+  # data.table is faster at reading large files
+  dat <- data.table::fread( input
+                            , colClasses=c("Activity.Code"="character")
+                            , na.strings = "") %>% as.data.frame() %>%
+    rename_extract_columns()
+
+  # print("Changing all characters to lower case ...")
+  # make sure all the values are lowercase
+  # dat[, sapply(dat, is.character)] <- lapply(dat[, sapply(dat, is.character)], tolower)
+
+  dat$system <- "dis"
+
+
+  # make_uic <- function(ic) {
+  #   ifelse(ic %in% c("eg.3.2-24", "eg.3.2-x17"), "eg.3.2-24/eg.3.2-x17"
+  #   , ifelse(ic %in% c("eg.3.2-25","eg.3.2-x18"), "eg.3.2-25/eg.3.2-x18"
+  #   , ifelse(ic %in% c("eg.3.2-26", "eg.3.2-x19"), "eg.3.2-26/eg.3.2-x19"
+  #   , ifelse(ic %in% c("eg.3.2-27","eg.3.2-x6"), "eg.3.2-27/eg.3.2-x6"
+  #   , ifelse(ic %in% c("eg.3.1-14/-15","eg.3.2-x22"),"eg.3.1-14/-15/eg.3.2-x22"
+  #   , ic)))))
+  # }
+
+  # dat$uic <- make_uic(dat$ic)
+  dat$actual <- as.numeric(dat$actual)
+  dat$target <- as.numeric(dat$target)
+  dat$uudn <- paste(dat$ic, dat$udn, sep = "_")
+  dat$commodity <- trimws(dat$commodity)
+
+  # dcw <- make_dis_crosswalk() #%>%
+  #   dplyr::mutate(ic = stringr::str_to_lower(ic))
+
+  # commodity_udns <- dcw %>%
+  #   dplyr::filter(if_any(ends_with("order"), ~ stringr::str_detect(., "\\["))) %>%
+  #   dplyr::distinct(ic, udn) %>%
+  #   dplyr::left_join(
+  #     dplyr::select(
+  #       dplyr::filter(
+  #         dat, !is.na(commodity)), ic, udn, commodity) %>%
+  #       dplyr::distinct())
+
+  dat$id <- NULL
+  # dat$first_order <- NULL
+  # dat$second_order <- NULL
+  # dat$third_order <- NULL
+  # dat$fourth_order <- NULL
+
+  dis <- as_tibble(dat) %>% left_join(disaggregate_crosswalk, relationship = "many-to-one")
+
+  return(dis)
+
+}
+
 
 #' @export rename_extract_columns
 #'
